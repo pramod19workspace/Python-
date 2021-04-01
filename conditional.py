98a04d48066552bbbc9b73a05884dd0e567cd1ab
Pramod R <pramod19workspace@gmail.com>
2021-04-01 16:02:36 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,9 @@
+a=1
+# if-elif-else ladder
+if a<3:
+    print("The value of a is less than 3")
+elif a>7:
+    print("The value of a is greater than 7")
+else:
+    print("The value of a is not greater than 3 or 7")
+print("done")
